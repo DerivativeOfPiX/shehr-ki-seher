dbaff1fed0c12af98ca66ef17b23221a2279e145
Hamza Asad <ma06762@st.habib.edu.pk>
2021-09-26 14:04:26 +0500

    bootstrap config

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+//import "../css/customcss.css";
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
